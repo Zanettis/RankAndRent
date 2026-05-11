f8e1c7ffa227755f97b788dd104209c1198ec5a2
Zanettis <zanettiaguiar@gmail.com>
2026-05-11 19:49:34 -0300

    Policy + Forms

--- murrieta-ecosystem/landscaping/astro.config.mjs
+++ murrieta-ecosystem/landscaping/astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://landscaping-murrieta.com',
-  output: 'static',
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [
     tailwind(),
     mdx(),
